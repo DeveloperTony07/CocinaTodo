1aa88ce2bb6eaf3d6f1fabc4816e778e7e92606f
unknown <tndeveloper07@gmail.com>
2023-05-01 00:16:24 -0600

    Tercera entrega se arreglo el diseno del Nav y se puso responsivo

--- dist/js/components/img-welcome.js
+++ dist/js/components/img-welcome.js
@@ -9,7 +9,14 @@ app.component('img-welcome',{
     ` <div class="content-fluid position-relative img-header">
     <img v-bind:src="image" class=" img-header img-fluid" alt="header">
     <div class="position-absolute top-50 start-50 translate-middle">
-        <h1 class="title-header">ENCUENTRA TODO <br> TIPO DE RECETAS</h1>
+    <h1 class="title-header">Encuentra Todo <br>Tipo De Recetas</h1>
+    <!--
+        <h1 class="title-header">
+        <span style='--content: "ENCUENTRA"; --start-color: #F23D4C; --end-color:#010A26; --delay: 0s;'>ENCUENTRA</span>
+        <span style='--content: "TODO TIPO"; --start-color: #F23D4C; --end-color:#010A26; --delay 3.33s;'>TODO TIPO</span>
+        <span style='--content: "DE RECETAS"; --start-color: #F23D4C; --end-color:#010A26; --delay 6.66s;'>DE RECETAS</span>
+        </h1>
+        -->
     </div>
     </div>`
 })
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data(){
         return{
             hola: "Hola Mundo",
-            image: "./imgs/header.jpg"
+            image: "./svgs/LogoWhite.svg"
         }
     }
 });
