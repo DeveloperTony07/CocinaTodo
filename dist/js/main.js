2071b1f071714b2687bb0617a7d67dbfa192e500
unknown <tndeveloper07@gmail.com>
2023-05-04 03:33:50 -0600

    Sexta entrega se agregaron los cards y se trabajo en el estilo de compnentes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return{
-            hola: "Hola Mundo",
             logoWhite: "./svgs/LogoWhite.svg",
             imgHeader: "./imgs/header.jpg"
         }
